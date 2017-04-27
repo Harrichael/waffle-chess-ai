ac863849e8483e881331539a72bb3e85e6201398
Michael Harrington <michaeltoddharrington@gmail.com>
2017-04-26 23:27:41 -0500

    timing changes

--- Games/Chess/AI.cs
+++ Games/Chess/AI.cs
@@ -107,7 +107,7 @@ namespace Joueur.cs.Games.Chess
                 this.engine.OpponentMove(fromFR, toFR, promote);
             }
             this.engine.Print();
-            int time = Math.Max( Math.Min( (int)Convert.ToInt32(this.Player.TimeRemaining/1000000) / Math.Max(120, this.Game.Moves.Count + 20), 5000), 250);
+            int time = Math.Max( Math.Min( (int)Convert.ToInt32(this.Player.TimeRemaining/1000000) / Math.Max(120 - this.Game.Moves.Count, 20), 2500), 250);
             var moveStrings = this.engine.MakeMove(time);
             Console.WriteLine("Move: " + moveStrings.Item1 + "    " + moveStrings.Item2);
             var piece = this.Player.Pieces.First(p => (p.File + p.Rank) == moveStrings.Item1);
